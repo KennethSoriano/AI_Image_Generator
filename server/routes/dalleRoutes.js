26ce502ca8435b4605de0716dffe4ef4569f064d
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2023-11-09 14:30:16 -0800

    dalleRoutes and postRoutes

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,9 +1,38 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { v2 as cloudinary } from 'cloudinary';
+import {  OpenAI } from 'openai';
 
-import Post from '../mongodb/models/post.js';
 
 dotenv.config();
 
 const router = express.Router();
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY 
+  });
+
+router.route('/').get((req, res) => {
+    res.send('hello kenny')
+});
+
+router.route('/').post(async (req, res) => {
+    try {
+        const { prompt } = req.body;
+
+        const aiResponse = await openai.createImage({
+            prompt,
+            n: 1,
+            size: '1024x1024',
+            response_format: 'b64_json',
+        });
+
+        const image = aiResponse.data.data[0].b64_json;
+
+        res.status(200).json({ photo: image});
+
+    } catch (error) {
+        res.status(500).send(error?.response.data.error.message)
+    }
+})
+
+export default router;
